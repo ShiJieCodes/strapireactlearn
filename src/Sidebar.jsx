0e5078822917040727d83c788035c809c9428b00
Lim Shi Jie <shijielim@gmail.com>
2023-09-27 01:35:53 +0800

    done

---
+++ src/Sidebar.jsx
@@ -0,0 +1,36 @@
+import { FaTimes } from "react-icons/fa";
+import sublinks from "./data";
+import { useGlobalContext } from "./context";
+
+const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className={isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}>
+      <div className="sidebar-container">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map(({ links, page, pageID }) => {
+            return (
+              <article>
+                <h4 key={pageID}>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map(({ id, label, icon, url }) => {
+                    return (
+                      <a href={url} key={id}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
+};
+export default Sidebar;
